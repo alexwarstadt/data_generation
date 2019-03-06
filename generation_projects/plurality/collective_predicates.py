b1c9defb0d931632d25d3f2d9392d62d78e867f6
hagenblix <40038070+hagenblix@users.noreply.github.com>
2019-03-05 21:38:03 -0500

    init for better conjugation

--- generation_projects/plurality/collective_predicates.py
+++ generation_projects/plurality/collective_predicates.py
@@ -1,14 +1,15 @@
+from utils.vocab_table import *
+import random
 # initialize output file
-output = open("../outputs/plurals/environment=collectivepredicates.tsv", "w")
-
+# output = open("../outputs/plurals/collectivepredicates.tsv", "w")
+output = open("collectivepredicates.tsv", "w")
 # set total number of paradigms to generate
 number_to_generate = 1000
 sentences = set()
 
 # gather word classes that will be accessed frequently
 all_irregular_nouns = get_all_conjunctive([("category", "N"), ("irrpl", "1")])
-all_irregular_nouns_sg = get_all(["sg", "1"])
-all_quantifiers = get_all("category", "(S/(S\\NP))/N")
-all_UE_quantifiers = get_all("restrictor_DE", "0", all_quantifiers)
-all_transitive_verbs = get_all("category", "(S\\NP)/NP")
-all_non_singular_nouns = np.append(get_all("pl", "1"), get_all("mass", "1"))
+all_irregular_nouns_sg = get_all("sg", "1", all_irregular_nouns)
+all_irregular_nouns_pl = get_all("pl", "1", all_irregular_nouns)
+# all_collective_predicates = get_all_conjunctive(["category", ""], ["collective_pred", ""])
+
