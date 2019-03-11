dca1722e380c48ef83142204a259ffa623fe47a4
Alex Warstadt <alexwarstadt@gmail.com>
2019-03-11 00:17:37 -0400

    Revert "paths are now relative"
    This reverts commit 8e69d7dfc1db34d982b62f12f5416e5ebdc20f0e.

---
+++ utils/vocab_table.py
@@ -0,0 +1,110 @@
+# Authors: Alex Warstadt
+# Functions for accessing vocab matrices
+
+import numpy as np
+from utils.data_type import data_type
+import os
+
+project_home = "/Users/alexwarstadt/Workspace/data_generation"
+project_home = "D:\Files\Work\GitHub\data_generation"
+vocab = np.genfromtxt(os.path.join(project_home, "vocabulary.csv"), delimiter=",", names=True, dtype=data_type)
+
+# def get_all(label, value):
+#     return np.array(list(filter(lambda x: x[label]==value, vocab)), dtype=data_type)
+
+def get_all(label, value, table=vocab):
+    """
+    :param label: string. field name.
+    :param value: string. label.
+    :param table: ndarray of vocab items.
+    :return: table restricted to all entries with "value" in field "label"
+    """
+    return np.array(list(filter(lambda x: x[label]==value, table)), dtype=data_type)
+
+def get_all_conjunctive(labels_values, table=vocab):
+    """
+    :param labels_values: list of (l,v) pairs: [(l1, v1), (l2, v2), (l3, v3)]
+    :return: vocab items with the given value for each label
+    """
+    to_return = table
+    for label, value in labels_values:
+        to_return = np.array(list(filter(lambda x: x[label] == value, to_return)), dtype=data_type)
+    return to_return
+
+
+def get_matches_of(row, label, table=vocab):
+    """
+    :param row: ndarray row. functor vocab item.
+    :param label: string. field containing selectional restrictions.
+    :param table: ndarray of vocab items.
+    :return: all entries in table that match the selectional restrictions of row as given in label.
+    """
+    value = str(np.array(row, dtype=data_type)[label])
+    if value == "":
+        pass
+    else:
+        matches = []
+        values = str(value).split(";")
+        for disjunct in values:
+            k_vs = conj_list(disjunct)
+            matches.extend(list(get_all_conjunctive(k_vs, table)))
+        return np.array(matches, dtype=data_type)
+
+
+def get_matched_by(row, label, table=vocab):
+    """
+    :param row: ndarray row. selected vocab item.
+    :param label: string. field containing selectional restrictions.
+    :param table: ndarray of vocab items.
+    :return: all entries in table whose selectional restrictions in label are matched by row.
+    """
+    matches = []
+    for entry in table:
+        value = str(np.array(entry, dtype=data_type)[label])
+        if is_match_disj(row, value):
+            matches.append(entry)
+    return np.array(matches)
+
+
+def conj_list(conjunction):
+    """
+    :param disjunct: a string corresponding to a conjunction of selectional restrictions
+    :return: a list of k, v pairs 
+    """
+    try:
+        to_return = [(v.split("=")[0], v.split("=")[1]) for v in conjunction.split("^")]
+        return to_return
+    except IndexError:
+        pass
+
+def is_match_disj(row, disjunction):
+    """
+    :param row: a vocab item
+    :param disjunction: a string corresponding to a disjunction of selectional restrictions
+    :return: true if the row matches one of the disjuncts, false otherwise
+    """
+    if disjunction == "":
+        return True
+    else:
+        disjuncts = disjunction.split(";")
+        match = False
+        for d in disjuncts:
+            match = match or is_match_conj(row, d)
+        return match
+
+def is_match_conj(row, conjunction):
+    """
+    :param row: a vocab item
+    :param conjunction: a string corresponding to a conjunction of selectional restrictions
+    :return: true if the row matches the conjunction, false otherwise
+    """
+    conjuncts = conj_list(conjunction)
+    match = True
+    for k, v in conjuncts:
+        try:
+            match = match and row[k] == v
+        except TypeError:
+            pass
+    return match
+
+
