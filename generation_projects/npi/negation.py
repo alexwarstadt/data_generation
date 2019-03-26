ea2a1bc8a553ff279389dd62b5f221227e156742
Alicia Parrish <parris39@msu.edu>
2019-03-26 11:18:42 -0400

    added monoclausal cases for ever and at-all

--- generation_projects/npi/negation.py
+++ generation_projects/npi/negation.py
@@ -11,8 +11,8 @@ import numpy as np
 
 # initialize output file
 rel_output_path = "outputs/npi/environment=negation.tsv"
-project_root = "/".join(os.path.join(os.path.dirname(os.path.abspath(__file__))).split("/")[:-2])
-#project_root = "G:/My Drive/NYU classes/Semantics team project seminar - Spring 2019/dataGeneration/data_generation"
+#project_root = "/".join(os.path.join(os.path.dirname(os.path.abspath(__file__))).split("/")[:-2])
+project_root = "G:/My Drive/NYU classes/Semantics team project seminar - Spring 2019/dataGeneration/data_generation"
 output = open(os.path.join(project_root, rel_output_path), "w")
 
 # set total number of paradigms to generate
@@ -170,6 +170,69 @@ while len(sentences) < number_to_generate:
     # write sentences to output
     if sentence_1 not in sentences:
         # sentences 1-4 have negation present
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=1_npi-present=1" % 1, sentence_1))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=1_npi-present=0" % 1, sentence_2))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=0_npi-present=1" % 0, sentence_3))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=0_npi-present=0" % 1, sentence_4))
+
+        # sentences 5-8 have no negation present
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=1_npi-present=1" % 0, sentence_5))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=1_npi-present=0" % 1, sentence_6))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=0_npi-present=1" % 0, sentence_7))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=0_npi-present=0" % 1, sentence_8))
+
+    # keep track of which sentences have already been generated
+    sentences.add(sentence_1)
+
+# -------------------------
+# Now monoclausal cases with "ever"
+
+sentences = set()
+
+# sample sentences until desired number
+while len(sentences) < number_to_generate:
+    # sentence template
+    # Neg_word  N1          aux  ever/often  V1      no/some  N2
+    # No        teachers    will ever        fail    some     students .
+
+    # build all lexical items
+    #TODO: throw in modifiers
+    N1 = choice(all_animate_nouns)
+    D1 = choice(get_matched_by(N1, "arg_1", all_common_dets))
+    Neg_word1 = choice(get_matched_by(N1, "arg_1", all_neg_det))
+    V1 = choice(get_matched_by(N1, "arg_1", all_transitive_verbs))
+    Aux1 = return_aux(V1, N1, allow_negated=False)
+    N2 = choice(all_animate_nouns)
+    #N2 = choice(get_matches_of(V1, "arg_2", all_non_singular_nouns))
+    D2 = choice(get_matched_by(N2, "arg_1", all_common_dets))
+    Neg_word2 = choice(get_matched_by(N2, "arg_1", all_neg_det))
+
+    # build sentences with licensor present
+    sentence_1 = "%s %s %s ever %s %s %s ." % (Neg_word1[0], N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_2 = "%s %s %s often %s %s %s ." % (Neg_word1[0], N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_3 = "%s %s %s ever %s %s %s ." % (D1[0], N1[0], Aux1[0], V1[0], Neg_word2[0], N2[0])
+    sentence_4 = "%s %s %s often %s %s %s ." % (D1[0], N1[0], Aux1[0], V1[0], Neg_word2[0], N2[0])
+
+    # build sentences with no licensor present
+    sentence_5 = "Some %s %s ever %s %s %s ." % (N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_6 = "Some %s %s often %s %s %s ." % (N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_7 = "%s %s %s ever %s some %s ." % (D1[0], N1[0], Aux1[0], V1[0], N2[0])
+    sentence_8 = "%s %s %s often %s some %s ." % (D1[0], N1[0], Aux1[0], V1[0], N2[0])
+
+    # remove doubled up spaces (this is because the bare plural doesn't have a determiner,
+    # but the code outputs a determiner with an empty string. might want to change this)
+    sentence_1 = remove_extra_whitespace(sentence_1)
+    sentence_2 = remove_extra_whitespace(sentence_2)
+    sentence_3 = remove_extra_whitespace(sentence_3)
+    sentence_4 = remove_extra_whitespace(sentence_4)
+    sentence_5 = remove_extra_whitespace(sentence_5)
+    sentence_6 = remove_extra_whitespace(sentence_6)
+    sentence_7 = remove_extra_whitespace(sentence_7)
+    sentence_8 = remove_extra_whitespace(sentence_8)
+
+    # write sentences to output
+    if sentence_1 not in sentences:
+        # sentences 1-4 have negation present
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=1_scope=1_npi-present=1" % 1, sentence_1))
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=1_scope=1_npi-present=0" % 1, sentence_2))
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=1_scope=0_npi-present=1" % 0, sentence_3))
@@ -180,6 +243,69 @@ while len(sentences) < number_to_generate:
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=0_scope=1_npi-present=0" % 1, sentence_6))
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=0_scope=0_npi-present=1" % 0, sentence_7))
         output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=ever_quantifier=%s_licensor=0_scope=0_npi-present=0" % 1, sentence_8))
+
+    # keep track of which sentences have already been generated
+    sentences.add(sentence_1)
+
+# -------------------------
+# Now monoclausal cases with "at all"
+
+sentences = set()
+
+# sample sentences until desired number
+while len(sentences) < number_to_generate:
+    # sentence template
+    # Neg_word  N1          aux  V1      no/some  N2        at all
+    # No        teachers    will fail    some     students  at all.
+
+    # build all lexical items
+    #TODO: throw in modifiers
+    N1 = choice(all_animate_nouns)
+    D1 = choice(get_matched_by(N1, "arg_1", all_common_dets))
+    Neg_word1 = choice(get_matched_by(N1, "arg_1", all_neg_det))
+    V1 = choice(get_matched_by(N1, "arg_1", all_transitive_verbs))
+    Aux1 = return_aux(V1, N1, allow_negated=False)
+    N2 = choice(all_animate_nouns)
+    #N2 = choice(get_matches_of(V1, "arg_2", all_non_singular_nouns))
+    D2 = choice(get_matched_by(N2, "arg_1", all_common_dets))
+    Neg_word2 = choice(get_matched_by(N2, "arg_1", all_neg_det))
+
+    # build sentences with licensor present
+    sentence_1 = "%s %s %s %s %s %s at all ." % (Neg_word1[0], N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_2 = "%s %s %s %s %s %s sometimes." % (Neg_word1[0], N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_3 = "%s %s %s %s %s %s at all." % (D1[0], N1[0], Aux1[0], V1[0], Neg_word2[0], N2[0])
+    sentence_4 = "%s %s %s %s %s %s sometimes." % (D1[0], N1[0], Aux1[0], V1[0], Neg_word2[0], N2[0])
+
+    # build sentences with no licensor present
+    sentence_5 = "Some %s %s %s %s %s at all." % (N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_6 = "Some %s %s %s %s %s sometimes." % (N1[0], Aux1[0], V1[0], D2[0], N2[0])
+    sentence_7 = "%s %s %s %s some %s at all." % (D1[0], N1[0], Aux1[0], V1[0], N2[0])
+    sentence_8 = "%s %s %s %s some %s sometimes." % (D1[0], N1[0], Aux1[0], V1[0], N2[0])
+
+    # remove doubled up spaces (this is because the bare plural doesn't have a determiner,
+    # but the code outputs a determiner with an empty string. might want to change this)
+    sentence_1 = remove_extra_whitespace(sentence_1)
+    sentence_2 = remove_extra_whitespace(sentence_2)
+    sentence_3 = remove_extra_whitespace(sentence_3)
+    sentence_4 = remove_extra_whitespace(sentence_4)
+    sentence_5 = remove_extra_whitespace(sentence_5)
+    sentence_6 = remove_extra_whitespace(sentence_6)
+    sentence_7 = remove_extra_whitespace(sentence_7)
+    sentence_8 = remove_extra_whitespace(sentence_8)
+
+    # write sentences to output
+    if sentence_1 not in sentences:
+        # sentences 1-4 have negation present
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=1_npi-present=1" % 1, sentence_1))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=1_npi-present=0" % 1, sentence_2))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=0_npi-present=1" % 1, sentence_3))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=1_scope=0_npi-present=0" % 1, sentence_4))
+
+        # sentences 5-8 have no negation present
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=1_npi-present=1" % 0, sentence_5))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=1_npi-present=0" % 1, sentence_6))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=0_npi-present=1" % 0, sentence_7))
+        output.write("%s\t%s\n" % ("experiment=NPI_env=negation_npi=at-all_quantifier=%s_licensor=0_scope=0_npi-present=0" % 1, sentence_8))
 
     # keep track of which sentences have already been generated
     sentences.add(sentence_1)
