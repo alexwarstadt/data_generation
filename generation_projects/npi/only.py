cbee14eb74159592eec66644b91aa14c3e8a4fc8
palomajeretic <paloma@nyu.edu>
2019-03-27 17:26:42 -0400

    update only

--- generation_projects/npi/only.py
+++ generation_projects/npi/only.py
@@ -14,7 +14,7 @@ project_root = "/".join(os.path.join(os.path.dirname(os.path.abspath(__file__)))
 output = open(os.path.join(project_root, rel_output_path), "w")
 
 # set total number of paradigms to generate
-number_to_generate = 20
+number_to_generate = 100
 sentences = set()
 
 # gather word classes that will be accessed frequently
@@ -24,34 +24,17 @@ all_transitive_verbs = get_all("category", "(S\\NP)/NP")
 all_non_singular_nouns = np.intersect1d(np.append(get_all("pl", "1"), get_all("mass", "1")), get_all("frequent", "1"))
 all_non_progressive_transitive_verbs = get_all("ing", "0", all_transitive_verbs)
 adverbs = ('always','sometimes','often', 'now')
-sentence_final_adv = ('sometimes', 'now', 'today', 'this year', 'this week')
-quantity_adv = ('a lot', 'little')
+sentence_final_adv = ('sometimes', 'today', 'this year', 'this week')
+quantity_adv = ('happily', 'angrily', 'appropriately', 'inappropriately')
 
 
 
-# TODO : fix "been"
 
 
-# repeat for "ever"
-
-# PITFALL:
-# ever doesn't occur with progressive
-# Every boy who has ever eaten a potato is tall.
-# *? Every boy who is ever eating a potato is tall.
-
-# PITFALL #2:
-# ever occurs after auxiliary "do"
-# The boy rarely ever did say that the girl wears jeans.
-# * The boy rarely did ever say that the girl wears jeans.
-
+# ever
 sentences = set()
 while len(sentences) < number_to_generate:
 
-    # sentence templates
-    # Only D1 N1 (Aux) n/ever V D2 N2.
-    # DP1 (Aux) n/ever V1 only DP2.
-    # DP1 (Aux) also n/ever V1 DP2.
-    # DP1 (Aux) n/ever also V1 DP2.
 
     N1 = choice(all_animate_nouns)
     D1 = choice(get_matched_by(N1, "arg_1", all_common_dets))
@@ -106,7 +89,7 @@ while len(sentences) < number_to_generate:
 
 
 
-
+#any
 sentences = set()
 while len(sentences) < number_to_generate:
 
@@ -165,6 +148,68 @@ while len(sentences) < number_to_generate:
         output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=any-licensor=0-scope=0-npi_present=0", 1, sentence_8))
 
     sentences.add(sentence_1)
+
+
+
+
+# at all
+sentences = set()
+while len(sentences) < number_to_generate:
+
+
+
+    N1 = choice(all_animate_nouns)
+    D1 = choice(get_matched_by(N1, "arg_1", all_common_dets))
+    V = choice(get_matched_by(N1, "arg_1", all_non_progressive_transitive_verbs))
+    Aux = return_aux(V, N1, allow_negated=False)
+    N2 = choice(get_matches_of(V, "arg_2", all_non_singular_nouns))
+    D2 = choice(get_matched_by(N2, "arg_1", all_common_dets))
+    q_adv = choice(quantity_adv)
+    fin_adv = choice(sentence_final_adv)
+
+    # sentence templates
+    # Only D1 N1 (Aux) V D2 N2 at all/q fin_adv
+    # D1 N1 (Aux) V D2 N2 at all/q only fin_adv
+    # D1 N1 (Aux) also V D2 N2 at all/q fin_adv
+    # D1 N1 (Aux) V D2 N2 at all/q also fin_adv
+
+    sentence_1 = "only %s %s %s %s %s %s at all %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], fin_adv)
+    sentence_2 = "only %s %s %s %s %s %s %s %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], q_adv, fin_adv)
+    sentence_3 = "%s %s %s %s %s %s at all only %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], fin_adv)
+    sentence_4 = "%s %s %s %s %s %s %s only %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], q_adv, fin_adv)
+
+    sentence_5 = "%s %s %s also %s %s %s at all %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], fin_adv)
+    sentence_6 = "%s %s %s also %s %s %s %s %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], q_adv, fin_adv)
+    sentence_7 = "%s %s %s %s %s %s at all also %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], fin_adv)
+    sentence_8 = "%s %s %s %s %s %s %s also %s ." % (D1[0], N1[0], Aux[0], V[0], D2[0], N2[0], q_adv, fin_adv)
+
+    # remove doubled up spaces (this is because of empty determiner AND EMPTY AUXILIARY).
+    sentence_1 = remove_extra_whitespace(sentence_1)
+    sentence_2 = remove_extra_whitespace(sentence_2)
+    sentence_3 = remove_extra_whitespace(sentence_3)
+    sentence_4 = remove_extra_whitespace(sentence_4)
+    sentence_5 = remove_extra_whitespace(sentence_5)
+    sentence_6 = remove_extra_whitespace(sentence_6)
+    sentence_7 = remove_extra_whitespace(sentence_7)
+    sentence_8 = remove_extra_whitespace(sentence_8)
+
+
+    # write sentences to output
+    if sentence_1 not in sentences:
+        # sentences 1-4 have only
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=1-scope=1-npi_present=1", 1, sentence_1))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=1-scope=1-npi_present=0", 1, sentence_2))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=1-scope=0-npi_present=1", 0, sentence_3))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=1-scope=0-npi_present=0", 1, sentence_4))
+
+        # sentences 5-8 don't have only
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=0-scope=1-npi_present=1", 0, sentence_5))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=0-scope=1-npi_present=0", 1, sentence_6))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=0-scope=0-npi_present=1", 0, sentence_7))
+        output.write("%s\t%d\t%s\n" % ("experiment=NPI-env=only-npi=yet-licensor=0-scope=0-npi_present=0", 1, sentence_8))
+
+    sentences.add(sentence_1)
+
 
 
 
