5b4535fdfd33cf6ddf90a4dc0dc75ddac4efa0a3
Alex Warstadt <alexwarstadt@gmail.com>
2019-04-12 22:37:45 -0400

    debug unify

--- output_processing/unify_test.py
+++ output_processing/unify_test.py
@@ -62,6 +62,7 @@ def get_results_dtype(args):
     if args.experiment_type == "npi_scope":
         dtype.extend([("in domain accuracy", "f8"), ("out of domain accuracy", "f8")])
         # dtype.extend([("cond_3_unacceptable", "f8"), ("cond_4_acceptable", "f8")])
+        dtype.extend([("1/1", "f8"), ("1/0", "f8"), ("0/1", "f8"), ("0/0", "f8")])
         for npi in ["any", "ever", "yet"]:
             dtype.extend([(npi+":1/1", "f8"), (npi+":1/0", "f8"), (npi+":0/1", "f8"), (npi+":0/0", "f8")])
     if args.experiment_type == "polar_q":
