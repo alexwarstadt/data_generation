fd1ee88a2050996b7af7a2d05ee11d47cab11c1b
Alicia Parrish <parris39@msu.edu>
2019-04-27 17:47:53 -0400

    updated subsetting to make 500 HITs with 5 sentences each

--- mturk_qc/sentences_subsetting.R
+++ mturk_qc/sentences_subsetting.R
@@ -14,7 +14,7 @@ for(i in c(1:length(filenames))){
 }
 
 # make tsv with all environments
-write.table(allData,"../outputs/npi/all_environments.tsv",sep='\t',col.names=NA)
+# write.table(allData,"../outputs/npi/all_environments.tsv",sep='\t',col.names=NA)
 
 names(allData) <- c('metadata','acceptability','blank','sentence')
 
@@ -28,91 +28,110 @@ Data2 = allData%>%
   mutate(rand_int=round(runif(nrow(allData),min=0,max=100000)))%>%
   arrange(rand_int)
 
-# troubleshotting
-nopara = function(x) (gsub("para.*","",x))
-simpQs<-allData%>%
-  filter(grepl("simplequestions",metadata))%>%
-  mutate(metadata=nopara(metadata))
+# subset to only 'ever' and 'any'
+Data3<-Data2%>%
+  filter(npi=='ever'|npi=='any')
 
-# should give 400 rows  
-examples = Data2[!duplicated(Data2[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+# should give 136 rows each
+examples1 = Data3[!duplicated(Data3[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+
+Data4<-Data3 %>% filter(!metadata %in% examples$metadata)
+examples2 <- Data4[!duplicated(Data4[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+
+Data5<-Data4 %>% filter(!metadata %in% examples2$metadata)
+examples3 <- Data5[!duplicated(Data5[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+
+# oversample from everything except sentential neg, determiner neg, only cases +80
+Data6<- Data5 %>% filter(environment!='sentential_negation_biclausal',
+                         environment!='determiner_negation_biclausal',
+                         environment!='only',
+                         !metadata %in% examples3$metadata)
+examples4<-Data6[!duplicated(Data6[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+
+# take 4 more random rows to make an even set
+Data7<-Data6 %>% filter(!metadata %in% examples4$metadata)
+examples_to_subset<-Data7[!duplicated(Data7[c("experiment","environment","npi","licensor","scope","npi_present")]),]
+examples5<-examples_to_subset[1:4,]
+
+# should be 500 rows
+all_examples<-rbind(examples1,examples2,examples3,examples4,examples5)
+
+# check for no duplicates in metadata --> should be 0
+nrow(all_examples[duplicated(all_examples["metadata"]),])
+
+# check for no duplicates in metadata --> should be 500
+length(unique(all_examples$metadata))
 
 # sanity check 
-see_env<-examples%>%
+see_env<-all_examples%>%
   group_by(environment)%>%
   summarise(count=n())
 
-# another sanity check
-see_neg<-examples%>%
-  filter(environment=="negation")
-
-# more sanity checking
-see_simpQ<-Data2%>%
-  filter(environment=="simplequestions")
-
 # sanity check that I'm pulling random paradigms
-see<-examples %>%
+see<-all_examples %>%
   group_by(paradigm)%>%
   summarise(count=n())
 
-# oversample from adverb cases +40 items
-more_adverbs = Data2 %>%
-  filter(environment == 'adverbs')%>%
-  filter(!metadata%in%examples$metadata)
-more_adverb_examples = more_adverbs[!duplicated(more_adverbs[c("npi","licensor","scope","npi_present")]),]
-
-# oversample from superlative cases +48
-more_supers = Data2 %>%
-  filter(environment == 'superlative')%>%
-  filter(!metadata%in%examples$metadata)
-more_super_examples = more_supers[!duplicated(more_supers[c("npi","licensor","scope","npi_present")]),]
-
-# pull an extra random 12 cases, so total number will =500 examples
-more_sentence_items = Data2 %>%
-  filter(!metadata%in%more_adverb_examples$metadata)%>%
-  filter(!metadata%in%more_super_examples$metadata)%>%
-  filter(!metadata%in%examples$metadata)
-more_sentences = more_sentence_items[!duplicated(more_sentence_items[c("npi","licensor","scope","npi_present")]),]
-more_sentence_examples = more_sentences[1:12,]
-
-# this should equal 500 items
-examples_full = rbind(more_adverb_examples,more_super_examples,more_sentence_examples,examples)
-
-#check for no duplicates in metadata --> should be 500 in the output.
-length(unique(examples_full$metadata))
-
-write.table(examples_full,"examples.tsv", sep='\t',col.names=NA)
+write.table(all_examples,"examples.tsv", sep='\t',col.names=NA)
 
 #--------------
-# separate into 25 smaller datasets
+# separate into smaller datasets of 4 sentences each
 
-examples_rand=examples_full%>%
-  mutate(rand_int=round(runif(nrow(examples_full),min=0,max=100000)))%>%
+examples_rand=all_examples%>%
+  mutate(metadata=paste0(metadata,"-acceptability=",acceptability))%>%
+  mutate(rand_int=round(runif(nrow(all_examples),min=0,max=100000)))%>%
   arrange(rand_int)
-examples=examples_rand[1:500,]
 
 attnChks = read.table("attention_checks.txt",sep='\t',header=T)
-  
+
 already_used = NULL
-for(i in c(1:25)){
-  newData = examples%>%
+FullTurkDataset = NULL
+for(i in c(1:125)){
+  newData = examples_rand%>%
     filter(!metadata%in%already_used$metadata)
   tempData = newData[!duplicated(newData[c("environment","npi")]),]
   print(paste0("tempData run",i,": ",nrow(tempData)))
-  if(nrow(tempData)>=20){
-    subsetTempData = tempData[1:20,]
+  if(nrow(tempData)>=4){
+    subsetTempData = tempData[1:4,]
   } else {
     print(paste0("using newData, run",i,": ",nrow(newData)))
-    subsetTempData = newData[1:20,] 
+    subsetTempData = newData[1:4,] 
   }
   print(paste0("subsetTempData run",i,": ",nrow(subsetTempData)))
+  print(paste0("accpetability of items in run ",i,": ",subsetTempData$acceptability))
   already_used = rbind(already_used,subsetTempData)
   dataFile = rbind(subsetTempData,attnChks)
-  turkDataFile = NULL
-  for(j in c(1:nrow(dataFile))){
-    turktemp = paste0("# trial ",j," ",dataFile$metadata[j],"-acceptability=",dataFile$acceptability[j],"\n",dataFile$sentence[j],"\n")
-    turkDataFile=rbind(turkDataFile,turktemp)
-  }
-  write.table(turkDataFile,paste0("dataset",i,".txt"), sep='\t',quote=F,col.names=F,row.names=F)
+  cond1 = as.character(dataFile$metadata[1])
+  cond2 = as.character(dataFile$metadata[2])
+  cond3 = as.character(dataFile$metadata[3])
+  cond4 = as.character(dataFile$metadata[4])
+  cond_attn = as.character(dataFile$metadata[5])
+  sent1 = as.character(dataFile$sentence[1])
+  sent2 = as.character(dataFile$sentence[2])
+  sent3 = as.character(dataFile$sentence[3])
+  sent4 = as.character(dataFile$sentence[4])
+  sent_attn = as.character(dataFile$sentence[5])
+  turkDataFile <- data.frame('list' = c(1:4),
+                           'item_1_condition'= c(cond1,cond2,cond3,cond4), #1243
+                           'field_1_1'= c(sent1,sent2,sent3,sent4),
+                           'item_2_condition' = c(cond2,cond3,cond1,cond4), #2314
+                           'field_2_1'= c(sent2,sent3,sent1,sent4),
+                           'item_3_condition' = c(cond3,cond4,cond2,cond1), #3421
+                           'field_3_1'= c(sent3,sent4,sent2,sent1),
+                           'item_4_condition' = c(cond_attn,cond_attn,cond_attn,cond_attn), # attn chks
+                           'field_4_1' = c(sent_attn,sent_attn,sent_attn,sent_attn),
+                           'item_5_condition' = c(cond4,cond1,cond3,cond2), #4132
+                           'field_5_1' = c(sent4,sent1,sent3,sent2))
+  FullTurkDataset=rbind(FullTurkDataset,turkDataFile)
 }
+
+# give unique identifier to each list
+FullTurkDataset$list<-seq.int(nrow(FullTurkDataset))
+
+# sanity check --> should give 500
+length(unique(FullTurkDataset$item_5_condition))
+# sanity check --> should give 1
+length(unique(FullTurkDataset$item_4_condition))
+
+write.csv(FullTurkDataset,"mturk_dataset.csv",row.names=F)
 
