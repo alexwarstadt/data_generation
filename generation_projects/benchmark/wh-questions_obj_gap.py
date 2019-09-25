5450002ec6ede600606d1b96a7e0ee68d3c813bd
Alicia-Parrish <parris39@msu.edu>
2019-09-25 12:08:09 -0400

    generation scripts for filler-gap paradigms

--- generation_projects/benchmark/wh-questions_obj_gap.py
+++ generation_projects/benchmark/wh-questions_obj_gap.py
@@ -11,7 +11,7 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
     def __init__(self):
         super().__init__(field="syntax",
                          linguistics="Filler-gap_dependency",
-                         uid="wh-questions_subj_gap",
+                         uid="wh-questions_obj_gap",
                          simple_lm_method=True,
                          one_prefix_method=False,
                          two_prefix_method=False,
@@ -19,11 +19,11 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
         self.embedding_verbs = get_all("category", "(S\\NP)/S")
 
     def sample(self):
-        # John noticed the man that saw everyone.
-        # N1   V1          N2  that V2  N3
+        # John noticed the cheese that the rat ate.
+        # N1   V1          N2     that     N3  V2
 
-        # John noticed who the man saw everyone.
-        # N1   V1      wh      N2  V2  N3
+        # John noticed what the rat ate the cheese.
+        # N1   V1      wh       N3  V2      N2
 
         V1 = choice(all_transitive_verbs)
         try:
@@ -31,20 +31,20 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
             N2 = N_to_DP_mutate(choice(get_matches_of(V1, "arg_2", all_common_nouns)))
         except TypeError:
             pass
-        V2 = choice(get_matched_by(N2, "arg_1", all_transitive_verbs))
+        V2 = choice(get_matched_by(N2, "arg_2", all_transitive_verbs))
         try:
-            N3 = N_to_DP_mutate(choice(get_matches_of(V2, "arg_2", all_nouns)))
+            N3 = N_to_DP_mutate(choice(get_matches_of(V2, "arg_1", all_nouns)))
         except TypeError:
             pass
 
         V1 = conjugate(V1, N1)
-        V2 = conjugate(V2, N2)
+        V2 = conjugate(V2, N3)
 
-        wh = choice(get_matched_by(N3, "arg_1", all_wh_words))
+        wh = choice(get_matched_by(N2, "arg_1", all_wh_words))
 
         data = {
-            "sentence_good": "%s %s %s that %s %s." % (N1[0], V1[0], N2[0], V2[0], N3[0]),
-            "sentence_bad": "%s %s %s %s %s %s." % (N1[0], V1[0], wh[0], N2[0], V2[0], N3[0])
+            "sentence_good": "%s %s %s that %s %s." % (N1[0], V1[0], N2[0], N3[0], V2[0]),
+            "sentence_bad": "%s %s %s %s %s %s." % (N1[0], V1[0], wh[0], N3[0], V2[0], N2[0])
         }
         return data, data["sentence_good"]
 
