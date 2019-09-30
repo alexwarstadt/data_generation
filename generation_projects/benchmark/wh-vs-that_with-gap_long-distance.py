3139c70afe7155431368de4fb13549da0e394e9b
Alicia-Parrish <parris39@msu.edu>
2019-09-30 17:37:59 -0400

    wh-vs-that paradigm gemeration scripts

--- generation_projects/benchmark/wh-vs-that_with-gap_long-distance.py
+++ generation_projects/benchmark/wh-vs-that_with-gap_long-distance.py
@@ -11,26 +11,27 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
     def __init__(self):
         super().__init__(field="syntax",
                          linguistics="Filler-gap_dependency",
-                         uid="wh-vs-that_no-gap",
+                         uid="wh-vs-that_with-gap",
                          simple_lm_method=True,
                          one_prefix_method=False,
                          two_prefix_method=False,
                          lexically_identical=False)
         self.embedding_verbs = get_all("category", "(S\\NP)/S")
+        self.robustly_transitive_verbs = get_all("strict_trans", "1")
 
     def sample(self):
-        # I  know that the lion devoured a gazelle.
-        # N1 V1   that     N2   V2         N3
+        # I  know what the lion devoured.
+        # N1 V1   wh       N2   V2
 
-        # I  know what the lion devoured a gazelle.
-        # N1 V1   wh       N2   V2         N3
+        # I  know that the lion   devoured.
+        # N1 V1   that       N2   V2
 
         V1 = choice(self.embedding_verbs)
         try:
             N1 = N_to_DP_mutate(choice(get_matches_of(V1, "arg_1", all_nouns)))
         except TypeError:
             pass
-        V2 = choice(all_transitive_verbs)
+        V2 = choice(self.robustly_transitive_verbs)
         try:
             N2 = N_to_DP_mutate(choice(get_matches_of(V2, "arg_1", all_common_nouns)))
             N3 = N_to_DP_mutate(choice(get_matches_of(V2, "arg_2", all_nouns)))
@@ -43,8 +44,8 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
         wh = choice(get_matched_by(N3, "arg_1", all_wh_words))
 
         data = {
-            "sentence_good": "%s %s that %s %s %s." % (N1[0], V1[0], N2[0], V2[0], N3[0]),
-            "sentence_bad": "%s %s %s %s %s %s." % (N1[0], V1[0], wh[0], N2[0], V2[0], N3[0])
+            "sentence_good": "%s %s %s %s %s." % (N1[0], V1[0], wh[0], N2[0], V2[0]),
+            "sentence_bad": "%s %s that %s %s." % (N1[0], V1[0], N2[0], V2[0])
         }
         return data, data["sentence_good"]
 
