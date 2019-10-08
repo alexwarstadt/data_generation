a702184ffb29a68556fe5e439cbae5e18efebad6
Haokun Liu <haokunliu412@gmail.com>
2019-10-07 22:29:01 -0400

    no-gap -> no_gap
    my editor automatically format the code, but it should work as usual

--- generation_projects/benchmark/wh_vs_that_no_gap.py
+++ generation_projects/benchmark/wh_vs_that_no_gap.py
@@ -9,13 +9,15 @@ from utils.vocab_sets import *
 
 class FillerGapGenerator(data_generator.BenchmarkGenerator):
     def __init__(self):
-        super().__init__(field="syntax",
-                         linguistics="filler_gap_dependency",
-                         uid="wh_vs_that_no-gap",
-                         simple_lm_method=True,
-                         one_prefix_method=False,
-                         two_prefix_method=False,
-                         lexically_identical=False)
+        super().__init__(
+            field="syntax",
+            linguistics="filler_gap_dependency",
+            uid="wh_vs_that_no_gap",
+            simple_lm_method=True,
+            one_prefix_method=False,
+            two_prefix_method=False,
+            lexically_identical=False,
+        )
         self.embedding_verbs = get_all("responsive", "1")
 
     def sample(self):
@@ -43,10 +45,15 @@ class FillerGapGenerator(data_generator.BenchmarkGenerator):
         wh = choice(get_matched_by(N3, "arg_1", all_wh_words))
 
         data = {
-            "sentence_good": "%s %s that %s %s %s." % (N1[0], V1[0], N2[0], V2[0], N3[0]),
-            "sentence_bad": "%s %s %s %s %s %s." % (N1[0], V1[0], wh[0], N2[0], V2[0], N3[0])
+            "sentence_good": "%s %s that %s %s %s."
+            % (N1[0], V1[0], N2[0], V2[0], N3[0]),
+            "sentence_bad": "%s %s %s %s %s %s."
+            % (N1[0], V1[0], wh[0], N2[0], V2[0], N3[0]),
         }
         return data, data["sentence_good"]
 
+
 generator = FillerGapGenerator()
-generator.generate_paradigm(rel_output_path="outputs/benchmark/%s.jsonl" % generator.uid)
+generator.generate_paradigm(
+    rel_output_path="outputs/benchmark/%s.jsonl" % generator.uid
+)
