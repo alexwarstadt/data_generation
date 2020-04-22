06ac69b642ee178778bd753a39f361ec203a0600
Alex Warstadt <alexwarstadt@gmail.com>
2020-04-22 15:07:53 -0400

    debug length

--- generation_projects/inductive_biases/person_length.py
+++ generation_projects/inductive_biases/person_length.py
@@ -49,52 +49,46 @@ class MyGenerator(generation_projects.inductive_biases.person_helper.PersonGener
         # Because S_SHORT,    John thinks    that the hairdresser found  them.
         # Adv     S_SHORT, D1 NP1  cp_verb_1 THAT D2  NP2         verb_2 non_first_acc
 
-        while True:
+        first, non_first, first_acc, non_first_acc = self.get_pronouns()
+        NP1 = choice(all_animate_nouns)
+        NP2 = choice(all_animate_nouns, avoid=NP1)
+        D1 = choice(get_matched_by(NP1, "arg_1", self.dets))
+        D2 = choice(get_matched_by(NP2, "arg_1", self.dets))
+        cp_verb = choice(self.cp_verb)
+        cp_verb_aux = return_aux(cp_verb, first)
+        cp_verb_first = re_conjugate(cp_verb, first, cp_verb_aux)
+        cp_verb_non_first = re_conjugate(cp_verb, non_first, cp_verb_aux)
+        cp_verb_1 = re_conjugate(cp_verb, NP1, cp_verb_aux)
+        verb = choice(self.trans_verb)
+        verb_aux = return_aux(verb, NP1)
+        try:
+            verb_1 = re_conjugate(verb, NP1, verb_aux)
+        except Exception:
+            pass
+        verb_2 = re_conjugate(verb, NP2, verb_aux)
+        main_clause = "%s %s that %s %s %s %s %s" % (first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0])
+        other_main_clause = "%s %s that %s %s %s %s %s" % (non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0])
 
-            first, non_first, first_acc, non_first_acc = self.get_pronouns()
-            NP1 = choice(all_animate_nouns)
-            NP2 = choice(all_animate_nouns, avoid=NP1)
-            D1 = choice(get_matched_by(NP1, "arg_1", self.dets))
-            D2 = choice(get_matched_by(NP2, "arg_1", self.dets))
-            cp_verb = choice(self.cp_verb)
-            cp_verb_aux = return_aux(cp_verb, first)
-            cp_verb_first = re_conjugate(cp_verb, first, cp_verb_aux)
-            cp_verb_non_first = re_conjugate(cp_verb, non_first, cp_verb_aux)
-            cp_verb_1 = re_conjugate(cp_verb, NP1, cp_verb_aux)
-            verb = choice(self.trans_verb)
-            verb_aux = return_aux(verb, NP1)
-            try:
-                verb_1 = re_conjugate(verb, NP1, verb_aux)
-            except Exception:
-                pass
-            verb_2 = re_conjugate(verb, NP2, verb_aux)
-            main_clause = "%s %s that %s %s %s %s %s" % (first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0])
-            other_main_clause = "%s %s that %s %s %s %s %s" % (non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0])
+        long_subordinate_clause, short_subordinate_clause = self.build_dependent_clauses(main_clause, other_main_clause)
 
-            try:
-                long_subordinate_clause, short_subordinate_clause = self.build_dependent_clauses(main_clause, other_main_clause)
-            except LengthHelperError as e:
-                print("\"%s\" is %s" % (e.sentence, e.too_long))
-                continue
+        data = self.build_paradigm(
+            training_1_1="%s, %s %s that %s %s %s %s %s." % (long_subordinate_clause[0], first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
+            training_0_0="%s, %s %s that %s %s %s %s %s." % (short_subordinate_clause[0], non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
+            test_1_0="%s, %s %s %s that %s %s %s %s." % (short_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
+            test_0_1="%s, %s %s %s that %s %s %s %s." % (long_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0]),
+            control_1_1="%s, %s %s %s that %s %s %s %s." % (long_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
+            control_0_0="%s, %s %s %s that %s %s %s %s." % (short_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0])
+        )
 
-            data = self.build_paradigm(
-                training_1_1="%s, %s %s that %s %s %s %s %s." % (long_subordinate_clause[0], first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
-                training_0_0="%s, %s %s that %s %s %s %s %s." % (short_subordinate_clause[0], non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
-                test_1_0="%s, %s %s %s that %s %s %s %s." % (short_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
-                test_0_1="%s, %s %s %s that %s %s %s %s." % (long_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0]),
-                control_1_1="%s, %s %s %s that %s %s %s %s." % (long_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
-                control_0_0="%s, %s %s %s that %s %s %s %s." % (short_subordinate_clause[0], D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0])
-            )
-
-            track_sentence = [
-                (first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
-                (non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
-                (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
-                (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0]),
-                (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
-                (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0])
-            ]
-            return data, track_sentence
+        track_sentence = [
+            (first[0], cp_verb_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
+            (non_first[0], cp_verb_non_first[0], D1[0], NP1[0], verb_1[0], D2[0], NP2[0]),
+            (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
+            (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0]),
+            (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], first_acc[0]),
+            (D1[0], NP1[0], cp_verb_1[0], D2[0], NP2[0], verb_2[0], non_first_acc[0])
+        ]
+        return data, track_sentence
 
 generator = MyGenerator()
 generator.generate_paradigm(number_to_generate=100, rel_output_path="outputs/inductive_biases/%s.jsonl" % generator.uid)
