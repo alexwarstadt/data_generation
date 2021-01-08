4be1e239a3bede9edfeb77cffa04294eb0238b09
Nigel Flower <neflower93@gmail.com>
2021-01-08 16:20:39 -0600

    added return aux method

--- utils/conjugate_db.py
+++ utils/conjugate_db.py
@@ -28,3 +28,29 @@ def conjugate(verb, subj, allow_negated=True, require_negated=False, change_v_fo
     verb[0] = aux[0] + " " + verb[0]
     return verb
 
+
+def return_aux(verb, subj, allow_negated=True, require_negated=False, allow_modal=True):
+    """
+    :param verb: vocab entry
+    :param subj: vocab entry
+    :param allow_negated: should negated auxiliaries (e.g. shouldn't) ever be generated?
+    :param require_negated: should negated auxiliaries always be generated?
+    :return: auxiliary that agrees with verb, or none if no auxiliary is needed.
+    """
+    if allow_negated and allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_modals_auxs)
+    if allow_negated and not allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_auxs)
+    if not allow_negated and allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_non_negated_modals_auxs)
+    if not allow_negated and not allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_non_negated_auxs)
+    if require_negated and allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_negated_modals_auxs)
+    if require_negated and not allow_modal:
+        safe_auxs = db.get_all_conjunctive(vocab.all_negated_auxs)
+
+    subj_auxs = db.get_matched_by(subj, "arg_1", safe_auxs, subtable=True)
+    aux = choice(db.get_matched_by(verb, "arg_2", subj_auxs, subtable=True))
+
+    return aux
