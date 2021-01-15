7e91208e344a9e1ba9e07e7a54f08a4c23413dba
Nigel Flower <neflower93@gmail.com>
2021-01-15 12:08:56 -0600

    added failing generation project

--- generation_projects/blimp/adjunct_island_db.py
+++ generation_projects/blimp/adjunct_island_db.py
@@ -1,22 +1,27 @@
-from utils import data_generator
-from utils.constituent_building import *
-from utils.conjugate import *
+#from utils import data_generator
+import utils.vocab_sets_db as db
+import utils.vocab_table_db as vocab
+
+from utils.constituent_building_db import *
+from utils.conjugate_db import *
 from utils.randomize import choice
-from utils.vocab_sets_db import *
-from utils.vocab_table_db import *
 
-class CSCGenerator(data_generator.BenchmarkGenerator):
+class CSCGenerator():#data_generator.BenchmarkGenerator):
     def __init__(self):
-        super().__init__(field="syntax",
-                         linguistics="island_effects",
-                         uid="adjunct_island",
-                         simple_lm_method=True,
-                         one_prefix_method=False,
-                         two_prefix_method=False,
-                         lexically_identical=True)
-
-        self.all_ing_transitives = all_transitive_verbs + all_ing_verbs
+        # super().__init__(field="syntax",
+        #                  linguistics="island_effects",
+        #                  uid="adjunct_island",
+        #                  simple_lm_method=True,
+        #                  one_prefix_method=False,
+        #                  two_prefix_method=False,
+        #                  lexically_identical=True)
+        all_transitive_verbs = db.get_all_conjunctive(vocab.all_transitive_verbs)
+        all_ing_verbs = db.get_all_conjunctive(vocab.all_ing_verbs)
+        self.all_ing_transitives = np.intersect1d(all_transitive_verbs, all_ing_verbs)
         self.adverbs = ["before", "while", "after", "without"]
+        self.all_nouns = db.get_all_conjunctive(vocab.all_nouns)
+        self.all_non_finite_transitive_verbs = db.get_all_conjunctive(vocab.all_non_finite_transitive_verbs)
+        self.all_wh_words = db.get_all_conjunctive(vocab.all_wh_words)
 
     def sample(self):
         # What did      John read  before filing the book?
@@ -32,12 +37,14 @@ class CSCGenerator(data_generator.BenchmarkGenerator):
         # Wh = choice(get_matched_by(Obj, "arg_1", all_wh_words))
         # Adv = choice(self.adverbs)
 
-        V_mat = choice(get_all_conjunctive(all_non_finite_transitive_verbs))
-        Subj = N_to_DP_mutate(choice(get_matches_of(V_mat, "arg_1", all_nouns)))
+        V_mat = choice(self.all_non_finite_transitive_verbs)
+        Subj = N_to_DP_mutate(choice(db.get_matches_of(V_mat, "arg_1", self.all_nouns)))
         Aux_mat = return_aux(V_mat, Subj, allow_negated=False)
-        Obj = N_to_DP_mutate(choice(get_matches_of(V_mat, "arg_2", all_nouns)))
-        V_emb = choice(get_matched_by(Obj, "arg_2"))
-        Wh = choice(get_matched_by(Obj, "arg_1", all_wh_words))
+        Obj = N_to_DP_mutate(choice(db.get_matches_of(V_mat, "arg_2", self.all_nouns)))
+        match_arg1 = db.get_matched_by(Subj, "arg_1", self.all_ing_transitives, subtable=True)
+        match_arg2 = db.get_matched_by(Obj, "arg_2", match_arg1, subtable=True)
+        V_emb = choice(match_arg2)
+        Wh = choice(db.get_matched_by(Obj, "arg_1", self.all_wh_words, subtable=True))
         Adv = choice(self.adverbs)
 
         data = {
@@ -47,5 +54,7 @@ class CSCGenerator(data_generator.BenchmarkGenerator):
         return data, data["sentence_good"]
 
 generator = CSCGenerator()
-generator.generate_paradigm(rel_output_path="outputs/blimp/%s.jsonl" % generator.uid)
-
+# generator.generate_paradigm(rel_output_path="outputs/blimp/%s.jsonl" % generator.uid)
+for i in range(100):
+    _, sentence = generator.sample()
+    print(sentence)
